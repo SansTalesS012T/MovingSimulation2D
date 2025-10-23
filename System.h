993b45119ac33e22ca8b92bf7f5bdd08f901820b
SansTalesS012T <Underverse678@gmail.com>
2025-10-23 18:02:58 +0700

    System class for control all of object

---
+++ System.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Obj.h"
+using namespace std;
+
+class System {
+private:
+    vector<Obj*> objectList;
+    int borderX, borderY, listSize;
+
+public:
+    System(int x, int y) {
+        this->objectList = vector<Obj*>();
+        this->listSize = 0;
+        this->borderX = x;
+        this->borderY = y;
+    }
+    
+    ~System() {
+        for(int i = 0; i < this->listSize; i++) {
+            delete this->objectList[i];
+        }
+    }
+
+    void addObject(Obj *a) {
+        this->objectList.push_back(a);
+        this->listSize += 1;
+    }
+
+    void display() {
+        
+    }
+
+    void simulate() {
+
+    }
+};
